bdd9299158b15cf91679b7879dbe19c143916372
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2023-07-18 11:57:05 -0600

    Update singleton__pattern.js

--- src/modules/singleton__pattern.js
+++ src/modules/singleton__pattern.js
@@ -1,27 +1,26 @@
 class LocalStorageManager {
-  constructor() {
+  constructor () {
     if (!LocalStorageManager.instance) {
-      LocalStorageManager.instance = this;
+      LocalStorageManager.instance = this
     }
-    return LocalStorageManager.instance;
+    return LocalStorageManager.instance
   }
 
   // Guardar un elemento en el Local Storage
-  setItem(key, value) {
-    localStorage.setItem(key, JSON.stringify(value));
+  setItem (key, value) {
+    localStorage.setItem(key, JSON.stringify(value))
   }
 
   // Obtener un elemento del Local Storage
-  getItem(key) {
-    const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) : null;
+  getItem (key) {
+    const item = localStorage.getItem(key)
+    return item ? JSON.parse(item) : null
   }
 
   // Eliminar un elemento del Local Storage
-  removeItem(key) {
-    localStorage.removeItem(key);
+  removeItem (key) {
+    localStorage.removeItem(key)
   }
 }
 
-export default LocalStorageManager;
-
+export default LocalStorageManager
