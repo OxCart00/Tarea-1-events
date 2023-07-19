d28f77621214d3243ccb739b139431d6d8ff6f99
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2023-07-18 22:10:12 -0600

    update

--- src/events.js
+++ src/events.js
@@ -1,174 +1,170 @@
-import { fetchData } from "./modules/fetch.js";
-import { buildGrid } from "./modules/grids.js";
-import LocalStorageManager from './modules/singleton__pattern.js';
+import { fetchData } from './modules/fetch.js'
+import { buildGrid } from './modules/grids.js'
+import LocalStorageManager from './modules/singleton__pattern.js'
 
-const localStorageManager = new LocalStorageManager();
+const localStorageManager = new LocalStorageManager()
 
-let localStorageProxy = new Proxy(localStorage, {
+const localStorageProxy = new Proxy(localStorage, {
   set: function (target, key, value) {
     // Convierte el valor a una cadena antes de guardarlo en el almacenamiento local
-    target.setItem(key, JSON.stringify(value));
-    return true;
+    target.setItem(key, JSON.stringify(value))
+    return true
   },
   get: function (target, key) {
     // Obtén el valor del almacenamiento local y conviértelo a su tipo original
-    const value = target.getItem(key);
-    return value ? JSON.parse(value) : null;
+    const value = target.getItem(key)
+    return value ? JSON.parse(value) : null
   }
-});
+})
 
-export async function storageInfo(API_URL, id) {
-  const cachedDataString = localStorageProxy[id];
+export async function storageInfo (API_URL, id) {
+  const cachedDataString = localStorageProxy[id]
 
   if (cachedDataString) {
-    buildGrid(localStorageProxy[id]);
+    buildGrid(localStorageProxy[id])
   } else {
-    const data = await fetchData(API_URL);
-    localStorageProxy[id] = data;
-    buildGrid(localStorageProxy[id]);
+    const data = await fetchData(API_URL)
+    localStorageProxy[id] = data
+    buildGrid(localStorageProxy[id])
   }
-};
-
+}
 
 // Función para el evento 'Interested'
-export function handleInterested(event) {
-  const interestedBtn = event.target;
-  const card = interestedBtn.parentNode;
-  card.classList.add('interested');
-
+export function handleInterested (event) {
+  const interestedBtn = event.target
+  const card = interestedBtn.parentNode
+  card.classList.add('interested')
 
   const interestedList = localStorageManager.getItem('Interested')
-  const key = document.querySelector('.active').id;
-  const eventId = event.target.id;
-  const events = localStorageManager.getItem(key);
-  const selectedEvent = events.find(element => element.id === eventId);
+  const key = document.querySelector('.active').id
+  const eventId = event.target.id
+  const events = localStorageManager.getItem(key)
+  const selectedEvent = events.find(element => element.id === eventId)
 
-  interestedBtn.remove();
+  interestedBtn.remove()
 
-  const interestedContainer = document.createElement('div');
-  interestedContainer.classList.add('interested-notify-container');
-  const message = document.createElement('p');
-  message.textContent = 'You are interested in going.';
+  const interestedContainer = document.createElement('div')
+  interestedContainer.classList.add('interested-notify-container')
+  const message = document.createElement('p')
+  message.textContent = 'You are interested in going.'
   interestedContainer.appendChild(message)
 
-  const link1 = document.createElement('a');
-  link1.textContent = 'Changed your mind?';
-  link1.href = '#';
+  const link1 = document.createElement('a')
+  link1.textContent = 'Changed your mind?'
+  link1.href = '#'
   link1.addEventListener('click', function (e) {
-    e.preventDefault();
-    card.removeChild(interestedContainer);
-    card.appendChild(interestedBtn);
-    const temp = interestedList.filter(element => element.id !== eventId);
-    localStorageManager.setItem('Interested', temp);
-  });
-  interestedContainer.appendChild(link1);
-  card.appendChild(interestedContainer);  
+    e.preventDefault()
+    card.removeChild(interestedContainer)
+    card.appendChild(interestedBtn)
+    const temp = interestedList.filter(element => element.id !== eventId)
+    localStorageManager.setItem('Interested', temp)
+  })
+  interestedContainer.appendChild(link1)
+  card.appendChild(interestedContainer)
 
   // Aquí puedes agregar la lógica adicional para el evento 'Interested'
-  const exist = interestedList.some(item => item.id === eventId);
+  const exist = interestedList.some(item => item.id === eventId)
   if (!exist) {
-    interestedList.push(selectedEvent);
-    localStorageManager.setItem('Interested', interestedList);
+    interestedList.push(selectedEvent)
+    localStorageManager.setItem('Interested', interestedList)
   }
   // ...
 }
 
 // Función para el evento 'Going'
-export function handleGoing(event) {
-  const goingBtn = event.target;
-  const card = goingBtn.parentNode;
-  let interestedBtn = card.querySelector('.interestedBtn');
-  card.classList.add('going');
+export function handleGoing (event) {
+  const goingBtn = event.target
+  const card = goingBtn.parentNode
+  let interestedBtn = card.querySelector('.interestedBtn')
+  card.classList.add('going')
 
-  const goingList = localStorageManager.getItem('Going');
-  const interestedList = localStorageManager.getItem('Interested');
-  const key = document.querySelector('.active').id;
-  const eventId = event.target.id;
-  const events = localStorageManager.getItem(key);
-  const selectedEvent = events.find(element => element.id === eventId);
+  const goingList = localStorageManager.getItem('Going')
+  const interestedList = localStorageManager.getItem('Interested')
+  const key = document.querySelector('.active').id
+  const eventId = event.target.id
+  const events = localStorageManager.getItem(key)
+  const selectedEvent = events.find(element => element.id === eventId)
 
-  goingBtn.remove();
+  goingBtn.remove()
 
   if (interestedBtn) {
-    interestedBtn.remove(); // Eliminar el botón 'Interested'
-  }else{
-    document.querySelector('.interested-notify-container').remove();
+    interestedBtn.remove() // Eliminar el botón 'Interested'
+  } else {
+    document.querySelector('.interested-notify-container').remove()
   }
 
-  const message = document.createElement('p');
-  message.textContent = 'You are going to this event';
-  card.appendChild(message);
+  const message = document.createElement('p')
+  message.textContent = 'You are going to this event'
+  card.appendChild(message)
 
-  const link = document.createElement('a');
-  link.textContent = 'Changed your mind?';
-  link.href = '#';
+  const link = document.createElement('a')
+  link.textContent = 'Changed your mind?'
+  link.href = '#'
   link.addEventListener('click', function (e) {
-    e.preventDefault();
-    card.removeChild(message);
-    card.removeChild(link);
-    interestedBtn = document.createElement('button');
-    interestedBtn.textContent = 'Interested';
-    interestedBtn.classList.add('interestedBtn');
-    interestedBtn.id = eventId;
-    interestedBtn.addEventListener('click', handleInterested);
-    card.appendChild(interestedBtn);
-    card.appendChild(goingBtn);
-    const temp = goingList.filter(element => element.id !== eventId);
-    localStorageManager.setItem('Going', temp);
-  });
-  card.appendChild(link);
+    e.preventDefault()
+    card.removeChild(message)
+    card.removeChild(link)
+    interestedBtn = document.createElement('button')
+    interestedBtn.textContent = 'Interested'
+    interestedBtn.classList.add('interestedBtn')
+    interestedBtn.id = eventId
+    interestedBtn.addEventListener('click', handleInterested)
+    card.appendChild(interestedBtn)
+    card.appendChild(goingBtn)
+    const temp = goingList.filter(element => element.id !== eventId)
+    localStorageManager.setItem('Going', temp)
+  })
+  card.appendChild(link)
 
   // Aquí puedes agregar la lógica adicional para el evento 'Going'
-  const exist = goingList.some(item => item.id === eventId);
+  const exist = goingList.some(item => item.id === eventId)
   if (!exist) {
-    goingList.push(selectedEvent);
-    localStorageManager.setItem('Going', goingList);
+    goingList.push(selectedEvent)
+    localStorageManager.setItem('Going', goingList)
   }
-  const cleanedList = interestedList.filter(item => item.id !== eventId);
-  localStorageManager.setItem('Interested', cleanedList);
+  const cleanedList = interestedList.filter(item => item.id !== eventId)
+  localStorageManager.setItem('Interested', cleanedList)
 
   // ...
 }
 
 // Función para el evento 'Favorite'
-export function handleFavorite(event) {
-  const favoriteBtn = event.target;
-  const card = favoriteBtn.parentNode;
-  card.classList.add('favorite');
+export function handleFavorite (event) {
+  const favoriteBtn = event.target
+  const card = favoriteBtn.parentNode
+  card.classList.add('favorite')
 
   const favoriteList = localStorageManager.getItem('Favorite')
-  const key = document.querySelector('.active').id;
-  const eventId = event.target.id;
-  const events = localStorageManager.getItem(key);
-  const selectedEvent = events.find(element => element.id === eventId);
+  const key = document.querySelector('.active').id
+  const eventId = event.target.id
+  const events = localStorageManager.getItem(key)
+  const selectedEvent = events.find(element => element.id === eventId)
 
-  favoriteBtn.remove();
+  favoriteBtn.remove()
 
-  const message = document.createElement('p');
-  message.textContent = 'You love this event';
-  card.appendChild(message);
+  const message = document.createElement('p')
+  message.textContent = 'You love this event'
+  card.appendChild(message)
 
-  const link = document.createElement('a');
-  link.textContent = 'Changed your mind?';
-  link.href = '#';
+  const link = document.createElement('a')
+  link.textContent = 'Changed your mind?'
+  link.href = '#'
   link.addEventListener('click', function (e) {
-    e.preventDefault();
-    card.removeChild(message);
-    card.removeChild(link);
-    card.appendChild(favoriteBtn);
-    const temp = favoriteList.filter(element => element.id !== eventId);
-    localStorageManager.setItem('Favorite', temp);
-  });
-  card.appendChild(link);
+    e.preventDefault()
+    card.removeChild(message)
+    card.removeChild(link)
+    card.appendChild(favoriteBtn)
+    const temp = favoriteList.filter(element => element.id !== eventId)
+    localStorageManager.setItem('Favorite', temp)
+  })
+  card.appendChild(link)
 
   // Aquí puedes agregar la lógica adicional para el evento 'Going'
-  const exist = favoriteList.some(item => item.id === eventId);
+  const exist = favoriteList.some(item => item.id === eventId)
   if (!exist) {
-    favoriteList.push(selectedEvent);
-    localStorageManager.setItem('Favorite', favoriteList);
+    favoriteList.push(selectedEvent)
+    localStorageManager.setItem('Favorite', favoriteList)
   }
-
-
 
   // ...
 }
--- src/main.js
+++ src/main.js
@@ -1,35 +1,31 @@
-import { createTabs, changeColor } from '../src/modules/tabs.js';
-import { storageInfo } from '../src/events.js';
-import { categories, yourEvents } from '../src/settings.js';
-import LocalStorageManager from './modules/singleton__pattern.js';
+import { createTabs, changeColor } from '../src/modules/tabs.js'
+import { storageInfo } from '../src/events.js'
+import { categories, yourEvents } from '../src/settings.js'
+import LocalStorageManager from './modules/singleton__pattern.js'
 
-const localStorageManager = new LocalStorageManager();
+const localStorageManager = new LocalStorageManager()
 
 if (localStorageManager.getItem('Going') === null) {
   yourEvents.forEach(category => {
     localStorageManager.setItem(category.id, [])
-  });
+  })
 }
-  
 
-createTabs(categories);
+createTabs(categories)
 
-const tabButtons = document.querySelectorAll('.tabButton');
-let API_URL;
-
+const tabButtons = document.querySelectorAll('.tabButton')
+let API_URL
 
 // Init
-changeColor(0);
-API_URL = `https://knassbani2.execute-api.us-east-2.amazonaws.com/events/music`;
-await storageInfo(API_URL, "music");
-
+changeColor(0)
+API_URL = 'https://knassbani2.execute-api.us-east-2.amazonaws.com/events/music'
+await storageInfo(API_URL, 'music')
 
 // Agrega un controlador de eventos al botón
 tabButtons.forEach(function (element, index) {
   element.addEventListener('click', async function () {
-    changeColor(index);
-    API_URL = `https://knassbani2.execute-api.us-east-2.amazonaws.com/events/${element.id}`;
-    await storageInfo(API_URL, element.id);
-  });
-});
-
+    changeColor(index)
+    API_URL = `https://knassbani2.execute-api.us-east-2.amazonaws.com/events/${element.id}`
+    await storageInfo(API_URL, element.id)
+  })
+})
